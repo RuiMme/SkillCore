29ac669841f48f5d35dcad98fba51d9708a7b0e0
lishaojian <lishaojian32@gmail.com>
2026-07-11 18:48:15 +0800

    Version 1.0.2
    修复了开启/关闭技能框按键绑定的描述文本;

--- src/main/java/com/rui/skillcore/client/keys/KeyBindings.java
+++ src/main/java/com/rui/skillcore/client/keys/KeyBindings.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class KeyBindings extends KeyBinding {
     public static final List<KeyBinding> KEY_BINDINGS = new ArrayList<KeyBinding>();
 
-    public static final KeyBinding OPEN_SKILL_SCREEN = new KeyBindings("open_skill_screen", KeyConflictContext.IN_GAME, KeyModifier.NONE, GLFW.GLFW_KEY_K, "key.category.skill");
+    public static final KeyBinding OPEN_SKILL_SCREEN = new KeyBindings("open_skill_screen", KeyConflictContext.IN_GAME, KeyModifier.NONE, GLFW.GLFW_KEY_K, "key.category.skillcore");
 
     public KeyBindings(String description, IKeyConflictContext keyConflictContext, KeyModifier keyModifier, int keyCode, String category) {
         super(String.format("key.%s.%s", LibMisc.MOD_ID, description), keyConflictContext, keyModifier, InputMappings.Type.KEYSYM, keyCode, category);
